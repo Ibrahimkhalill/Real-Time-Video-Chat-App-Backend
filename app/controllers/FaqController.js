34bcc4c59d15c4ad4fc1c982011bac909606db4a
Ibrahimkhalill <mdibrahimkhalil516@gmail.com>
2024-09-17 19:21:34 +0600

    Your commit message

--- app/controllers/FaqController.js
+++ app/controllers/FaqController.js
@@ -1,23 +1,27 @@
 const { Live } = require("../models/index");
 
-
-
 // Function to upload a video and get the video ID
 
 // Function to upload a single video
 async function CreateRoom(req, res) {
   try {
     const roomData = req.body;
+    const { live_link, live_name } = roomData;
 
+    // Delete all existing rooms in the Live table
+    await Live.deleteMany({});
+    console.log("All previous rooms deleted");
+
+    // Create the new room
     const roomSaveData = await Live.create({
-      live_link: roomData.live_link,
-      live_name: roomData.live_name,
+      live_link,
+      live_name,
     });
 
-    console.log("Data inserted successfully:", roomSaveData);
+    console.log("New room created successfully:", roomSaveData);
     res.status(200).json(roomSaveData);
   } catch (error) {
-    console.error("Error inserting data:", error);
+    console.error("Error handling room creation:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 }
